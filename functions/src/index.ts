708593d7f0a34532bb2bd0dd9d56d6e276af02f5
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-03 16:46:37 +0530

    BASIC firebase api setup done

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,16 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import express = require("express");
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+/* *
+
+Basic Sample API
+
+*/
+const app = express();
+app.get("/timestamp", (request, response) => {
+  response.send(`${Date.now()}` + " Hello World");
+});
+exports.apphost = functions.https.onRequest(app);
