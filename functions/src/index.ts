ff076b8973622e9f7e19d2159203fd6c4ccc5914
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-05 16:15:38 +0530

    creating new forms and testing things

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,39 @@
 import * as functions from "firebase-functions";
 import express = require("express");
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+import admin = require("firebase-admin");
+const serviceAccount = require("../../ServiceAccountKey.json");
 
-/* *
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-Basic Sample API
+const uid = "some-uid";
+const additionalClaims = {
+  premiumnAccount: true
+};
 
-*/
+// admin
+//   .auth()
+//   .createCustomToken(uid, additionalClaims)
+//   .then(customToken => {
+//     console.log(customToken);
+//   })
+//   .catch(err => {
+//     console.log("Error Creating custom token:" + err);
+//   });
+// admin
+//   .auth()
+//   .createUser({
+//     phoneNumber: "+919819254711",
+//     password: "vino@123456",
+//     disabled: false
+//   })
+//   .then(userRecord => {
+//     console.log("successfully created: ", JSON.stringify(userRecord));
+//   })
+//   .catch(err => console.log("id creation error : " + JSON.stringify(err)));
+
 const app = express();
 app.get("/timestamp", (request, response) => {
   response.send(`${Date.now()}` + " Hello World");
