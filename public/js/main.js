18b2c0f6602b058d1c7efb97e3a2baa7b88d2738
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-05 17:33:44 +0530

    basic function created

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+function registerSubmit() {
+    console.log(JSON.stringify(document.getElementById(registerForm).value))
+}
+
+function generateOtp(mobilenumber) {
+    console.log(JSON.stringify(mobilenumber))
+}
+
+function otpSubmit(otp) {
+    console.log(JSON.stringify(otp))
+}
